3a7978ea9f12631c58ae614aaae4c2c86f1266f1
mateuszkrzak <madeye@o2.pl>
2017-01-27 13:29:40 +0100

    fix typos

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
 
 @Component({
   selector: 'app-search-result',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['search-result.component.scss'],
   inputs: ['productCategories:products']
 })
+export class SearchResultComponent  {
 
+}
