46ca2f4f5c9eaaf69001d667b2d2931fa12e2ece
mateuszkrzak <madeye@o2.pl>
2017-01-31 03:22:46 +0100

    clean console logs and comments

--- src/app/shared/models/product.ts
+++ src/app/shared/models/product.ts
@@ -13,14 +13,3 @@ export class Product {
     public requiredAlternativeAddonIds: number[]
   ) { }
 }
-// id	int	Identyfikator
-// carrierLogoLink	string	Adres do logotypu przewoźnika
-// name	string	Nazwa
-// carrierName	string	Nazwa przewoźnika
-// netPrice	float	Cena bazowa netto
-// grossPrice	float	Cena bazowa brutto
-// protocolAvailable	bool	Czy jest dostępna generacja protokołu zbiorczego /order/protocol
-// collectionTypes	CollectionType[]	Dostępne dla danego produktu typy nadania
-// transportType	string	Typ transportu
-// requiredAddonIds	int[]	Lista wymaganych dodatków do produktu. Wymiary podane do wyszukiwarki mogą spowodować iż aby złożyć zamówienie trzeba będzie poza dodatkami wybranymi przez klienta przekazać również wybór wskazanych na liście dodatków obowiązkowych.
-//   requiredAlternativeAddonIds	[int[]]	Lista wymaganych alternatywnych zestawów dodatków do produktu. Tablica zawiera tablice identyfikatorów dodatków, spośród których użytkownik musi wybrać przynajmniej jeden aby zamówienie zostało przyjęte.
--- src/app/shared/models/productCategories.ts
+++ src/app/shared/models/productCategories.ts
@@ -9,9 +9,3 @@ export class ProductCategories {
 
   ) { }
 }
-
-// standard	Product[]	Produkty standardowe
-// noon	Product[]	Produkty możliwe do realizacji do południa kolejnego dnia. Dostępność produktu należy potwierdzić wykonując ponownie wycenę z polem receiverPostCode, jeżeli pole te nie było wcześniej przekazane
-// morning	Product[]	Produkty możliwe do realizacji rano kolejnego dnia. Dostępność produktu należy potwierdzić wykonując ponownie wycenę z polem receiverPostCode, f pole te nie było wcześniej przekazane
-// fast	Product[]	Produkty możliwe do realizacji od 5 do 10 godzin. Pole pojawia się po przekazaniu pól senderPostCode oraz receiverPostCode
-// superfast	Product[]	Produkty możliwe do realizacji od 3 do 6 godzin. Pole pojawia się po przekazaniu pól senderPostCode oraz receiverPostCode
